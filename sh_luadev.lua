92a078fdd8d22ac39012aa176d8d1015d61b8e49
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2022-09-23 21:51:57 +0000

    correctly handle return values with nil (REQUIRES SFEX CHANGE) This change requires that StarfallEx itself be updated to not stop at first nil in return values for 'pcall' and 'xpcall'.

--- sh_luadev.lua
+++ sh_luadev.lua
@@ -4,36 +4,46 @@ return function(sfc3)
 		if func ~= nil and not isfunction(func) then
 			func, err = nil, func
 		end
-		return func, err
-	end
-	function sfc3.luadev_eval(identifier, code, executor, print_result)
-		local func, err
-		if print_result then
-			func, err = sfc3.luadev_loadstring('return '..code, "Validation")
-		end
 		if func == nil then
-			func, err = sfc3.luadev_loadstring(' '..code, "Validation")
-		end
-		if func == nil then
-			err = tostring(err)
-			return false, err, true
-		end
-		local retvals = {pcall(func)}
-		if not retvals[1] then
-			err = retvals[2]
-			if type(err) == 'table' then
+			if istable(err) then
 				err = rawget(err, 'message')
 			end
 			err = tostring(err)
-			return false, err
+		end
+		return func, err
+	end
+	function sfc3.luadev_retval(success, ...)
+		if not success then
+			local err = ...
+			if istable(err) then
+				err = rawget(err, 'message')
+			end
+			err = tostring(err)
+			return success, err
+		end
+		local t = {}
+		for i=1, select('#', ...) do
+			t[i] = tostring(select(i, ...))
+		end
+		return success, table.concat(t, "\t")
+	end
+	function sfc3.luadev_eval(identifier, code, executor, print_result)
+		local name, func, err = "Validation"
+		if print_result then
+			func, err = sfc3.luadev_loadstring('return '..code, name)
+		end
+		if func == nil then
+			func, err = sfc3.luadev_loadstring(' '..code, name)
+		end
+		if func == nil then
+			return false, err, true
+		end
+		local success, retval = sfc3.luadev_retval(pcall(func))
+		if not success then
+			return false, retval
 		end
 		if print_result then
-			table.remove(retvals, 1)
-			for i=1, #retvals do
-				retvals[i] = tostring(retvals[i])
-			end
-			retvals = table.concat(retvals, "\t")
-			return true, retvals
+			return true, retval
 		end
 		return true
 	end
